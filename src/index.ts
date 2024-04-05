a166f1c4963992d7eb03643816a1aea7d7d53f3a
Nunu <fravalo.killian2@gmail.com>
2024-04-05 11:49:03 +0200

    Default Routes Made
    May need to add some later in the project.

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,50 @@ const port = process.env.PORT || 8080;
 
 app.use(express.json())
 
-// app.use('/api/users', userRouter)
-// app.use('/api/posts', postRouter)
-// app.use('/api/comments', commentRouter)
-// app.use('/api/likes', likeRouter)
-
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.post('/register', (req, res) => {
+    //TODO: implement the register logic
+    return res.send('User registered successfully!');
+});
+
+app.post('/login', (req, res) => {
+    //TODO: implement the login logic
+    return res.send('User logged in successfully!');
+})
+
+app.get('/feed', (req, res) => {
+    //TODO: get the feed
+    return res.send('Feed fetched successfully!');
+});
+
+app.get('/store/:id', (req, res) => {
+    //TODO: get the store of a specific id
+    return res.send('Restaurant' + req.params.id + ' fetched successfully!');
+});
+
+app.get('/store/:id/plat', (req, res) => {
+    //TODO: get the menu of a specific store
+    return res.send('menu from the Restaurant!' + req.params.id + ' fetched successfully!');
+});
+
+app.get('/order/:id', (req, res) => {
+    //TODO: get the order
+    return res.send('Order number ' + req.params.id + ' fetched successfully!');
+});
+
+app.get('/account', (req, res) => {
+  //TODO: only display the account page if the user is logged in
+})
+
+app.get('/account/:id', (req, res) => {
+    //TODO: get the account infos to display in the account page
+});
+
+
+
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`)
