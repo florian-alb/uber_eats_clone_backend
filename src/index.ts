8b8d9a3ddbab36e51894e5bd1f47801b823ad93c
DepressedNunu <110053182+DepressedNunu@users.noreply.github.com>
2024-04-15 09:24:35 +0200

    102 creer un router express (#3)
    * Default Routes Made
    
    May need to add some later in the project.
    
    * Routes Split into different files
    
    May need to add some later in the project.
    
    * Fixed Routes / Default CRUD done

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
 import express from "express";
+import addressRouter from "./router/address.router";
+import categoryRouter from "./router/category.router";
+import menuRouter from "./router/menu.router";
+import orderRouter from "./routes/order";
+import productRouter from "./router/product.router";
+import restaurantRouter from "./router/restaurant.router";
+import reviewRouter from "./router/review.router";
+import userRouter from "./router/user.router";
 
-// import userRouter from "./routes/user.router";
-// import postRouter from "./routes/post.router";
-// import commentRouter from "./routes/comment.router";
-// import likeRouter from "./routes/like.router";
 
 const app = express()
 const port = process.env.PORT || 8080;
 
+// import Page
 app.use(express.json())
-
-// app.use('/api/users', userRouter)
-// app.use('/api/posts', postRouter)
-// app.use('/api/comments', commentRouter)
-// app.use('/api/likes', likeRouter)
+app.use('/address/', addressRouter)
+app.use('/category/', categoryRouter)
+app.use('/menu/', menuRouter)
+app.use('/order/', orderRouter)
+app.use('/product/', productRouter)
+app.use('/shop/', restaurantRouter)
+app.use('/review/', reviewRouter)
+app.use('/user/', userRouter)
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Test API')
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`)
+    console.log(`Server running on port: http://localhost:${port}`)
 })
