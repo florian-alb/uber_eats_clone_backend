dd943fa254a1d8ec93e8e88f013c8d5d0ed78f35
Florian <79457058+florian-alb@users.noreply.github.com>
2024-05-23 23:47:31 +0200

    cors

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const app = express()
 const port = process.env.PORT || 8080;
 
 app.use(cors({
-    origin: ['http://localhost:5173' , 'https://uber-eats-clone-frontend.vercel.app/'], // frontend origin
+    origin: ['http://localhost:5173' , 'https://uber-eats-clone-frontend.vercel.app'], // frontend origin
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: [
         'Content-Type',
